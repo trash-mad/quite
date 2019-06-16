b2b0e8f78e3cc51172396680d0e4dd84cd498b16
tripolskypetr <tripolskypetr@gmail.com>
2019-06-16 20:26:15 +0300

    console fix

---
+++ quite/extensions/consoleextension.cpp
@@ -0,0 +1,6 @@
+#include "consoleextension.h"
+
+ConsoleExtension::ConsoleExtension()
+{
+
+}
---
+++ quite/extensions/consoleextension.h
@@ -0,0 +1,11 @@
+#ifndef CONSOLEEXTENSION_H
+#define CONSOLEEXTENSION_H
+
+
+class ConsoleExtension
+{
+public:
+    ConsoleExtension();
+};
+
+#endif // CONSOLEEXTENSION_H
