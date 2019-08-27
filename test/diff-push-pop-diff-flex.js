aa4b4e88ba7ef0859a8c78eae66ec7394c491337
Petr Tripolsky <tripolskypetr@gmail.com>
2019-08-27 19:37:46 +0300

    some test

--- test/diff-push-pop-diff-flex.js
+++ test/diff-push-pop-diff-flex.js
@@ -6,19 +6,19 @@ function SampleComponent(props) {
 }
 
 SampleComponent.prototype.render=function() {
-    return Quite.createElement("Rectangle", {color: "white", flexDirection: "column", marginTop: 5, marginLeft: 5},
+    return Quite.createElement("Rectangle", {color: "yellow", flexDirection: "column", marginTop: 5, marginLeft: 5},
         Quite.createElement("Rectangle", {height: 55, color: "whitesmoke", flexDirection: "row", minHeight: 50, paddingTop: 5},
             Quite.createElement("Button", {text: "Add rect", height: 50, width: 125, marginRight:5, onClick: this.addRect}),
             Quite.createElement("Button", {text: "Remove rect", height: 50, width: 125, onClick: this.removeRect}),
         ), 
-        Quite.createElement("Rectangle", {color: "cyan", flexDirection: "column", minHeight: 150, minWidth: 150},
+        Quite.createElement("Rectangle", {color: "red", flexDirection: "column", minHeight: 150, minWidth: 150},
             this.state.rects.map((_, index)=>Quite.createElement("Rectangle", {
                         key: index+1,
                         height: 50, 
                         width: 50, 
-                        color: "cyan", 
+                        color: "green", 
                         marginRight: 5
-                    },
+            }/*,
                 Quite.createElement("Rectangle", {key:59,minHeight:15, minWidth:15, color:"red"},
                     Quite.createElement("Rectangle", {key:60,minHeight:15, minWidth:15, color:"red"}),
                     Quite.createElement("Rectangle", {key:61,minHeight:15, minWidth:15, color:"red"}),
@@ -26,7 +26,7 @@ SampleComponent.prototype.render=function() {
                         Quite.createElement("Rectangle", {key:63,minHeight:15, minWidth:15, color:"red"})
                     )
                 ),
-                Quite.createElement("Rectangle", {key: 88, minHeight:63, minWidth:15, color:"red"})
+                Quite.createElement("Rectangle", {key: 88, minHeight:63, minWidth:15, color:"red"})*/
             ))
         ),
     );
